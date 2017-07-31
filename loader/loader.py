a215c59b80b5d5a881f2e31b6c3bbe694ec4817c
Sean Shahkarami <sean.shahkarami@gmail.com>
2017-07-31 15:18:04 -0500

    added cassandra model support and data create test

--- loader/loader.py
+++ loader/loader.py
@@ -1,5 +1,27 @@
+from cassandra.cqlengine import columns
+from cassandra.cqlengine import connection
+from cassandra.cqlengine.management import sync_table
+from cassandra.cqlengine.management import create_keyspace_simple
+from cassandra.cqlengine.models import Model
 import pika
 import ssl
+from datetime import datetime
+
+
+class SensorData(Model):
+
+    node_id = columns.Text(partition_key=True)
+    date = columns.Date(partition_key=True)
+    plugin_id = columns.Text(primary_key=True)
+    created_at = columns.DateTime(primary_key=True)
+    topic = columns.Text()
+    data = columns.Blob()
+
+
+connection.setup(['127.0.0.1'], 'waggle')
+create_keyspace_simple('waggle', replication_factor=3)
+sync_table(SensorData)
+
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
     host='rabbitmq',
@@ -23,7 +45,15 @@ channel = connection.channel()
 
 
 def process_message(ch, method, properties, body):
-    print(properties, body, flush=True)
+    timestamp = datetime.now()
+
+    SensorData.create(
+        node_id='testnode',
+        date=timestamp.date(),
+        plugin_id='testplugin:0.1',
+        created_at=datetime.now(),
+        data=body)
+
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
